e41dc89bd13d7f8591caa37cad327b550c518e23
vintagechord85 <vintagechord85@gmail.com>
2025-10-21 22:36:50 +0900

    feat: set home to resources/views/public/home.blade.php

--- app/Http/Controllers/PublicHomeController.php
+++ app/Http/Controllers/PublicHomeController.php
@@ -1,11 +1,10 @@
 <?php
 namespace App\Http\Controllers;
-use Illuminate\Http\RedirectResponse;
 
 class PublicHomeController extends Controller
 {
-    public function __invoke(): RedirectResponse
+    public function __invoke()
     {
-        return redirect()->route('inquiry.create');
+        return view('public.home'); // ← 기존 랜딩
     }
 }

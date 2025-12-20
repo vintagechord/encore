2810f3926e0e5ca2247e9b98cbcb371b58812e42
vintagechord85 <vintagechord85@gmail.com>
2025-12-21 05:25:39 +0900

    Trust proxy to keep https asset URLs

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'admin.gate' => \App\Http\Middleware\AdminGate::class,
         ]);
+
+        // Trust Render's proxy so https is detected correctly.
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //

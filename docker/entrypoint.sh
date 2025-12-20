072e5d9d6fdfceaf6098b534a182bf73a3c2eee3
vintagechord85 <vintagechord85@gmail.com>
2025-12-21 05:07:50 +0900

    Remove old pivot migrations before auto-migrate

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,6 +2,8 @@
 set -e
 
 if [ "${SKIP_MIGRATIONS:-0}" != "1" ]; then
+  rm -f /var/www/html/database/migrations/2025_09_24_123450_create_artist_genre_table.php \
+        /var/www/html/database/migrations/2025_09_24_123450_create_artist_tag_table.php
   php /var/www/html/artisan migrate --force
 fi
 
